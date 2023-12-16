08d8f7422a15c5e9b8180766ca5b3529c9b4e70b
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-12-16 08:14:23 +0000

    Create destination folders before copying
    https://github.com/simeononsecurity/FireFox-Privacy-Script/issues/7

--- sos-firefoxprivacy.sh
+++ sos-firefoxprivacy.sh
@@ -1,14 +1,27 @@
-# /bin/bash
+#!/bin/bash
+
+# Function to display messages in red
+RED() {
+    echo -e "\e[91m$1\e[0m"
+}
 
 # Testing if root...
-if [ $UID -ne 0 ]
-then
-	RED "You must run this script as root!" && echo
-	exit
+if [ $UID -ne 0 ]; then
+    RED "You must run this script as root!" && echo
+    exit
 fi
 
-mkdir -p /lib/firefox/browser/defaults/preferences/
-mkdir -p /lib/firefox/distribution/extensions/
-cp ./Files/browser/defaults/preferences/* /lib/firefox/browser/defaults/preferences/*
-cp ./Files/distribution/extensions/* /lib/firefox/distribution/extensions/*
-cp ./Files/browser/distribution/* /lib/firefox/distribution/*
+# Define the directories
+preferences_dir="/lib/firefox/browser/defaults/preferences/"
+distribution_dir="/lib/firefox/distribution/"
+extensions_dir="/lib/firefox/distribution/extensions/"
+
+# Create directories if they don't exist
+mkdir -p "$preferences_dir"
+mkdir -p "$extensions_dir"
+mkdir -p "$distribution_dir"
+
+# Copy files to the directories
+cp ./Files/browser/defaults/preferences/* "$preferences_dir"
+cp ./Files/browser/distribution/* "$distribution_dir"
+cp ./Files/distribution/extensions/* "$extensions_dir"
