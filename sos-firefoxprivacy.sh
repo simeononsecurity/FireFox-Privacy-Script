135a00dc564ce3ed95e010118d0bdbcb0d4253b3
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-02-02 03:36:28 -0600

    Update sos-firefoxprivacy.sh
    https://github.com/simeononsecurity/FireFox-Privacy-Script/issues/11

--- sos-firefoxprivacy.sh
+++ sos-firefoxprivacy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Function to display messages in red
-RED() {
-    echo -e "\e[91m$1\e[0m"
+# Function to display messages in red and exit
+die() {
+    echo -e "\e[91m$1\e[0m" >&2
+    exit 1
 }
 
 # Testing if root...
 if [ $UID -ne 0 ]; then
-    RED "You must run this script as root!" && echo
-    exit
+    die "You must run this script as root!"
 fi
 
 # Define the directories
@@ -16,6 +16,11 @@ config_dir="/lib/firefox/"
 preferences_dir="/lib/firefox/browser/defaults/preferences/"
 distribution_dir="/lib/firefox/distribution/"
 extensions_dir="/lib/firefox/distribution/extensions/"
+
+# Check if config_dir exists
+if [ ! -d "$config_dir" ]; then
+    die "The specified Firefox directory does not exist. Firefox may not be installed or not in the default directory."
+fi
 
 # Create directories if they don't exist
 mkdir -p "$preferences_dir"
